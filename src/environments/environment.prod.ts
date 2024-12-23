856158cd05c1c920c248521bf4f7e984665e3f7b
ALABI MUHYDEEN OLANIYI <48595796+muhydeen95@users.noreply.github.com>
2024-12-23 12:47:05 +0100

    updated the route

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  api_url: '',
+  api_url: 'https://countriesnow.space/api/v0.1',
   AES_SecretKey: `#?t?v?VX?\r??z@????Vfk?u\u007f?^?\fms\u0011?`,
   basic_auth: btoa(`pass:surveyauthpass`),
 };
